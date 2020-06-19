99f30a0c3b7eedee7cb0819c1a8e33096f26c06a
Ganesh Shinde <GShinde@valencehealth.com>
2020-06-19 16:42:50 +0530

    new branch add

---
+++ Practice3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 16:23:47 2020
+
+@author: NGPawar
+"""
+
+def first_fun():
+    print('hello')
+    return 
+
+first_fun()
+
+a = 'Ganesh'
+b = 'Shinde'
+c = a + '-' + b
+
+print(c)
+
+print("We are learning Git")
+print ("xz"
